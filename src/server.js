439a3ba70bf7f0cec3dcde0ffdd56dec54c308dd
Muhammad Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-04-07 14:40:29 +0800

    change PORT config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const Hapi = require('@hapi/hapi')
 const routes = require('./routes')
 
+const PORT = process.env.PORT || 9000
+
 const init = async () => {
   const server = Hapi.server({
-    port: 9000,
+    port: PORT,
     host: 'localhost',
     routes: {
       cors: {
