d991d0c375bc19c2bce5457bc7bc11655db9e62b
Muhammad Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-04-07 15:19:19 +0800

    change config

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 9000
 const init = async () => {
   const server = Hapi.server({
     port: PORT,
-    host: 'azmi-bookshelf-api.herokuapp.com',
     routes: {
       cors: {
         origin: ['*']
