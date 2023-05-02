5a0d0c4f832cd53890f6712d8ba4b5fb50597000
Muhammad Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-05-02 14:26:59 +0800

    return port and host

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const Hapi = require('@hapi/hapi')
 const routes = require('./routes')
 
-const PORT = process.env.PORT || 9000
+const PORT = 9000
 
 const init = async () => {
   const server = Hapi.server({
     port: PORT,
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*']
